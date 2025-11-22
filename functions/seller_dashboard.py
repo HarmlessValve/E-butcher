02b08a252a0fd2e9e77070a74f2c449852a8281e
HarmlessValve <t4j42007@gmail.com>
2025-11-22 13:22:00 +0700

    testing seller functions

--- functions/seller_dashboard.py
+++ functions/seller_dashboard.py
@@ -1,8 +1,32 @@
 import pyfiglet as pf
 import questionary as qu
 from colorama import Fore as fr, Style as st
+from functions.connection import conn
 
-def dashboard():
+def dashboard(username, password):
     msg = pf.figlet_format("Seller Dashboard", font="slant")
     print(fr.GREEN + st.BRIGHT + msg + st.RESET_ALL)
-    return qu.select("dashboard menus", choices=["Account", "Product", "Order", "Recap", "Exit"]).ask()
+    option = qu.select("dashboard menus", choices=["Account", "Product", "Order", "Recap", "Exit"]).ask()
+
+    if option == "Account":
+        try:
+            connection, cursor = conn()
+            query = """
+            SELECT s.seller_name, s.phone_num, s.username, s.password, a.street_name, d.district_name 
+            FROM sellers s JOIN addresses a 
+            on s.address_id = a.address_id 
+            JOIN districts d
+            on a.district_id = d.district_id
+            WHERE username = %s and password = %s"""
+            cursor.execute(query, (username, password))
+            result = cursor.fetchone()
+            print(result)
+            
+            cursor.close()
+            connection.close()
+                
+            return result
+        except Exception as e:
+            
+            print("Error:", e)
+            return False
